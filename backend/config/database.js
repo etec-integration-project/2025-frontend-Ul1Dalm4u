4a133e7c87f64b06b77d09c9e1d95cc5bc58251f
Ul1Dalm4u <u.dalmau@alumno.etec.um.edu.ar>
2025-04-24 22:18:22 -0300

    Docker Funcional

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,32 +1,14 @@
-require('dotenv').config(); // Cargar las variables de entorno
-
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,      // Nombre de la base de datos
-  process.env.DB_USER,      // Usuario
-  process.env.DB_PASSWORD,  // Contraseña
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    retry: {
-      max: 10,
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  }
-);
+// Configura la conexión a MySQL
+const sequelize = new Sequelize('tino_uli', 'root', '1234', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
 
-// Probar la conexión
+// Verifica si la conexión es exitosa
 sequelize.authenticate()
-  .then(() => console.log('✅ Conexión exitosa a MySQL'))
-  .catch(err => {
-    console.error('❌ Error al conectar a MySQL:', err);
-    process.exit(1);
-  });
+  .then(() => console.log('Conexión exitosa a MySQL'))
+  .catch(err => console.error('Error al conectar a MySQL:', err));
 
 module.exports = sequelize;
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,9 @@
-require('dotenv').config(); // Carga las variables de entorno
-
 const express = require('express');
 const sequelize = require('./config/database');
 const productRoutes = require('./routes/products');
 const userRoutes = require('./routes/users'); 
+const cors= require('cors');
 const imageRoutes = require('./routes/images');
-const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,22 +17,13 @@ app.get('/', (req, res) => {
   res.send('Bienvenido a la tienda de ropa tino_uli');
 });
 
-// Rutas
+// Rutas para productos
 app.use('/products', productRoutes);
 app.use('/users', userRoutes);
 app.use('/images', imageRoutes);
 
-// Conexión a la base de datos y levantado del servidor
-sequelize.authenticate()
-  .then(() => {
-    console.log('✅ Conexión a la base de datos establecida correctamente.');
-    return sequelize.sync();
-  })
-  .then(() => {
-    app.listen(PORT, '0.0.0.0', () => {
-      console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error('❌ No se pudo conectar a la base de datos:', err.message);
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
   });
+});
